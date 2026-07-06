73dbd974f576daf78fa85e8357e8b20478234e7a
Julia <julia.reynolds@stcu.org>
2026-07-06 16:30:51 -0700

    added CLI functionality
    used events on user input for practice working with events

--- ATMMachine/ATMCLI.cs
+++ ATMMachine/ATMCLI.cs
@@ -2,8 +2,42 @@
 
 public class ATMCLI
 {
+    public static event Action? WithdrawSelected;
+    public static event Action? ViewCurrentStateSelected;
+
     public static void Main(string[] args)
     {
+        InputHandler input = new();
+
         Console.WriteLine("Welcome to the ATM");
+        Console.WriteLine();
+
+        bool repeat = true;
+
+        while (repeat)
+        {
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("1) View the current state of the ATM");
+            Console.WriteLine("2) Make a withdrawl");
+            Console.WriteLine("3) Exit");
+            Console.Write(">  ");
+
+            switch (Console.ReadLine())
+            {
+                case "1" or "1)" or "1.":
+                    ViewCurrentStateSelected?.Invoke();
+                    break;
+                case "2" or "2)" or "2.":
+                    WithdrawSelected?.Invoke();
+                    break;
+                case "3" or "3)" or "3." or "Q" or "Q." or "q" or "q."
+                or "end" or "End" or "exit" or "Exit" or "quit" or "Quit":
+                    repeat = false;
+                    break;
+                default:
+                    Console.WriteLine("InvalidInput\r\n>  ");
+                    break;
+            }
+        }
     }
 }
---
+++ ATMMachine/InputHandler.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATMMachine;
+
+internal class InputHandler
+{
+    ATM atm;
+
+    public InputHandler()
+    {
+        ATMCLI.WithdrawSelected += WithdrawHandled;
+        ATMCLI.ViewCurrentStateSelected += ViewCurrentStateHandled;
+
+        this.atm = new();
+    }
+    
+
+    private void WithdrawHandled()
+    {
+        Console.WriteLine();
+        Console.WriteLine("How much would you like to withdraw?");
+        Console.Write(">  ");
+
+        int withdrawlAmt = -1;
+
+        try
+        {
+            withdrawlAmt = int.Parse(Console.ReadLine());
+        }
+        catch
+        {
+            Console.WriteLine("Invalid input. Please enter an integer > 0");
+        }
+
+        Dictionary<int, int>? result = this.atm.Withdraw(withdrawlAmt);
+
+        if (result is null)
+        {
+            Console.WriteLine("\r\nThere are not enough funds in this ATM to complete " +
+                "your request at this time. Please visit the next nearest ATM.\r\n");
+
+            return;
+        }
+
+        Console.WriteLine("\r\nWithdraw Successful!");
+        PrintResult(result);
+    }
+
+    private void PrintResult(Dictionary<int, int> result)
+    {
+        Console.WriteLine();
+        
+        var total = 0;
+
+        foreach (var value in result.Keys)
+        {
+            var quantity = result[value];
+            total += (value * quantity);
+
+            Console.WriteLine($"{quantity} ${value} {atm.GetCurrencyType(value)}{((quantity > 1) ? "s" : "")}");
+        }
+
+        Console.WriteLine($"\r\nTotal:  {total}\r\n");
+    }
+
+    private void ViewCurrentStateHandled()
+    {
+        Console.WriteLine();
+        Console.WriteLine("| Value | Type | quantity of units |");
+        Console.WriteLine("|-------|------|-------------------|");
+
+        var total = 0;
+        var currentState = atm.GetCurrentState();
+
+        foreach (var value in currentState.Keys)
+        {
+            var quantity = currentState[value];
+            total += (value * quantity);
+
+            Console.WriteLine($"| {value}\t| {atm.GetCurrencyType(value)} | {quantity}\t           |");
+        }
+
+        Console.WriteLine($"\r\nMax Withdrawl Amount:  {total}\r\n");
+    }
+}

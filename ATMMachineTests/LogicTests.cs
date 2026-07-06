212c7b9b58661f93677a3b8908a781dd0592e7f4
Julia <julia.reynolds@stcu.org>
2026-07-06 14:01:29 -0700

    tests that withdrawls correctly subtract from amount remaining

--- ATMMachineTests/LogicTests.cs
+++ ATMMachineTests/LogicTests.cs
@@ -41,6 +41,54 @@ public class LogicTests
     }
 
     [Test]
+    public void ReturnsNegativeWhenAttemptingToRetirieveAmtOfInvalidCurrency()
+    {
+        // Arrange
+        ATM sut = new();
+
+        // Act
+        var available = sut.GetAvailable(-5);
+
+        // Assert
+        available.Should().BeLessThan(0);
+    }
+
+    [Test]
+    public void ReturnsCorrectAvailableOfCurrencyType()
+    {
+        // Arrange
+        ATM sut = new();
+
+        // Act
+        var available = sut.GetAvailable(500);
+
+        // Assert
+        available.Should().Be(2);
+    }
+
+    [Test]
+    public void ReturnsCurrentStateOfATM()
+    {
+        // Arrange
+        ATM sut = new();
+
+        // Act
+        var result = sut.GetCurrentState();
+
+        // Assert
+        result.Should().NotBeNull();
+        result[500].Should().Be(2);
+        result[200].Should().Be(3);
+        result[100].Should().Be(5);
+        result[50].Should().Be(12);
+        result[20].Should().Be(20);
+        result[10].Should().Be(50);
+        result[5].Should().Be(100);
+        result[2].Should().Be(250);
+        result[1].Should().Be(500);
+    }
+
+    [Test]
     public void WithdrawReturnsCorrectAmount()
     {
         // Arrange
@@ -60,5 +108,54 @@ public class LogicTests
 
         // Assert
         sum.Should().Be(434);
+    }
+
+    [Test]
+    public void ReturnsNullIfNotEnoughCash()
+    {
+        // Arrange
+        ATM sut = new();
+
+        // Act
+        var result = sut.Withdraw(99999999);
+
+        // Assert
+        result.Should().BeNull();
+    }
+
+    [Test]
+    public void OnlyWithdrawsExistingCash()
+    {
+        // Arrange
+        ATM sut = new();
+
+        // Act
+        Dictionary<int, int>? result = sut.Withdraw(5100);
+
+        // Assert
+        result.Should().NotBeNull();
+        result[500].Should().Be(2);
+        result[200].Should().Be(3);
+        result[100].Should().Be(5);
+        result[50].Should().Be(12);
+        result[20].Should().Be(20);
+        result[10].Should().Be(50);
+        result[5].Should().Be(100);
+        result[2].Should().Be(250);
+        result[1].Should().Be(500);
+    }
+
+    [Test]
+    public void UpdatesRemainingCashAfterWithdrawl()
+    {
+        // Arrange
+        ATM sut = new();
+
+        // Act
+        sut.Withdraw(1);
+
+        // Assert
+        sut.GetCurrentState()[1].Should().Be(499);
+        sut.GetAvailable(1).Should().Be(499);
     }
 }

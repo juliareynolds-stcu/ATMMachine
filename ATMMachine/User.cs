c2b076a9861bf1f7bfbc725c6bc888b62a92feed
Julia <julia.reynolds@stcu.org>
2026-07-08 09:45:35 -0700

    setup checking account and pocket for user

--- ATMMachine/User.cs
+++ ATMMachine/User.cs
@@ -7,7 +7,7 @@ namespace ATMMachine;
 public class User
 {
     private ATM pocket;
-    private int checkingBalance;
+    private double checkingBalance;
 
     private static readonly Dictionary<double, int> defaultPocket = new() { [100] = 1, [20] = 5, [5] = 3, [1] = 5, [.25] = 2 };
     private static readonly int defaultCheckingBalance = 1000;
@@ -34,5 +34,91 @@ public class User
                 this.pocket.Deposit(value, pocket[value]);
             }
         }
+    }
+
+    public double GetCheckingBalance()
+    {
+        return this.checkingBalance;
+    }
+
+    public bool DepositIntoChecking(double amount)
+    {
+        if (amount < 0)
+        {
+            return false;
+        }
+
+        this.checkingBalance = Math.Round((this.checkingBalance + amount), 2);
+
+        return true;
+    }
+
+    public bool DepositIntoChecking(double value, int quantity)
+    {
+        if ((value < 0) || (quantity < 0) 
+            || (this.pocket.GetCurrencyType(value) is null))
+        {
+            return false;
+        }
+
+        return DepositIntoChecking(value * quantity);
+    }
+
+    public double WithdrawFromChecking(double amount)
+    {
+        if (amount < 0)
+        {
+            return 0;
+        }
+
+        amount = Math.Round(amount, 2);
+
+        var newBalance = Math.Round((this.checkingBalance - amount), 2);
+
+        if (newBalance < 0)
+        {
+            return 0;
+        }
+
+        this.checkingBalance = newBalance;
+
+        return amount;
+    }
+
+    public double GetPocketBalance()
+    {
+        return this.pocket.GetTotalAvailable();
+    }
+
+    public bool PlaceInPocket(double value, int quantity)
+    {
+        if ((value < 0) || (quantity < 0))
+        {
+            return false;
+        }
+
+        var result = this.pocket.Deposit(value, quantity);
+
+        return result;
+    }
+
+    public Dictionary<double, int>? RemoveFromPocket(double amount)
+    {
+        if (amount < 0)
+        {
+            return null;
+        }
+
+        return this.pocket.Withdraw(Math.Round(amount, 2));
+    }
+
+    public Dictionary<double, int>? RemoveFromPocket(double value, int quantity)
+    {
+        if ((value < 0) || (quantity < 0))
+        {
+            return null;
+        }
+
+        return this.pocket.Withdraw(value, quantity);
     }
 }
